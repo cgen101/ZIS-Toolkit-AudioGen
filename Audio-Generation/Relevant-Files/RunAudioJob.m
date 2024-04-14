d4d6eccbcf910883d4df51fa845b92b13cc21a76
cgen101 <c_g638@txstate.edu>
2024-04-14 18:29:32 -0500

    Modify output message

--- Audio-Generation/Relevant-Files/RunAudioJob.m
+++ Audio-Generation/Relevant-Files/RunAudioJob.m
@@ -2,17 +2,17 @@
 %   files 
 
 % Path to directory containing files to read to/write from 
-ioPath = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\Relevant-Files";
+ioPath = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\Test-Dir-Fork\ZIS-toolkit-AudioGen\Audio-Generation\Relevant-Files";
 
 % Path to first audio file for test 1
-filePath1 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\non_colocated_highway.flac";
+filePath1 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\Test-Dir-Fork\ZIS-toolkit-AudioGen\Audio-Generation\non_colocated_highway.flac";
 % Path to second audio file for test 1
-filePath2 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\non_colocated_idle_2.flac";
+filePath2 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\Test-Dir-Fork\ZIS-toolkit-AudioGen\Audio-Generation\non_colocated_idle_2.flac";
 
 % Path to first audio file for test 2
-filePath3 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\colocated_city.flac";
+filePath3 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\Test-Dir-Fork\ZIS-toolkit-AudioGen\Audio-Generation\colocated_highway.flac";
 % Path to second audio file for test 2
-filePath4 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\colocated_city_2.flac";
+filePath4 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\Test-Dir-Fork\ZIS-toolkit-AudioGen\Audio-Generation\colocated_highway_2.flac";
 
 newAudioJob(filePath1, filePath2, ioPath);
 
--- Audio-Generation/Relevant-Files/determine-colocation.py
+++ Audio-Generation/Relevant-Files/determine-colocation.py
@@ -5,18 +5,20 @@ import json
 
 # Load JSON data from the file
 # CHANGE FILEPATH HERE
-filePath = r'C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\Relevant-Files\Results\cross_correlation_result.json'
+filePath = r'C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\Test-Dir-Fork\ZIS-toolkit-AudioGen\Audio-Generation\Relevant-Files\Results\cross_correlation_result_2.json'
 with open(filePath, 'r') as json_file:
     data = json.load(json_file)
 
 # Extract cross-correlation value from the JSON data
 cross_correlation = data.get('crossCorrelation', 0.0)  # Default to 0.0 if key not found
+fileName1 = ''.join(data.get('file1', []))
+fileName2 = ''.join(data.get('file2', []))
 
 # Define the threshold for colocation (you can adjust this threshold as needed)
 threshold = 0.011 # Example threshold value
 
 # Determine colocation status based on the threshold
 if cross_correlation >= threshold:
-    print("The devices are colocated based on the cross-correlation value.")
+    print(f"The files '{fileName1}' and '{fileName2}' are colocated based on the cross-correlation value.")
 else:
-    print("The devices are not colocated based on the cross-correlation value.")
+    print(f"The files '{fileName1}' and '{fileName2}' are not colocated based on the cross-correlation value.")
